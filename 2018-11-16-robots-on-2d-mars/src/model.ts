2ec7eda24aadd50165660db92f53462a75e451d6
Robbie Coomber <robbie@robbiecoomber.com>
2018-11-16 17:57:01 +0000

    First pass at creating a type for the game state

---
+++ 2018-11-16-robots-on-2d-mars/src/model.ts
@@ -0,0 +1,32 @@
+
+interface Vector2 {
+    x: Number
+    y: Number
+}
+
+enum Direction {
+    North,
+    South,
+    East,
+    West
+}
+
+interface Robot {
+    position: Vector2,
+    direction: Direction
+}
+
+interface WorldState {
+    maxX: Number,
+    maxY: Number,
+
+    currentRobot: Robot
+    pastRobots: Robot[]
+
+    deathScents: {
+        north: Set<Number>
+        south: Set<Number> // y = 0
+        west: Set<Number> // x = 0
+        east: Set<Number>
+    }
+}
