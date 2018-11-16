190f43a4affa7bf2da07698e3fdabfa477c0e340
Robbie Coomber <robbie@robbiecoomber.com>
2018-11-16 18:10:39 +0000

    Add prettier

--- 2018-11-16-robots-on-2d-mars/src/model.ts
+++ 2018-11-16-robots-on-2d-mars/src/model.ts
@@ -1,32 +1,31 @@
-
 interface Vector2 {
-    x: Number
-    y: Number
+  x: Number;
+  y: Number;
 }
 
 enum Direction {
-    North,
-    South,
-    East,
-    West
+  North,
+  South,
+  East,
+  West
 }
 
 interface Robot {
-    position: Vector2,
-    direction: Direction
+  position: Vector2;
+  direction: Direction;
 }
 
 interface WorldState {
-    maxX: Number,
-    maxY: Number,
+  maxX: Number;
+  maxY: Number;
 
-    currentRobot: Robot
-    pastRobots: Robot[]
+  currentRobot?: Robot;
+  pastRobots: Robot[];
 
-    deathScents: {
-        north: Set<Number>
-        south: Set<Number> // y = 0
-        west: Set<Number> // x = 0
-        east: Set<Number>
-    }
+  deathScents: {
+    north: Set<Number>;
+    south: Set<Number>; // y = 0
+    west: Set<Number>; // x = 0
+    east: Set<Number>;
+  };
 }
