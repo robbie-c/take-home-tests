fc5e59bb90bc4d1758d99ac1741b099a9c7a8245
Robbie Coomber <robbie@robbiecoomber.com>
2018-11-19 16:15:41 +0000

    Add some negative test cases to parse first line

--- 2018-11-16-robots-on-2d-mars/src/controller.test.ts
+++ 2018-11-16-robots-on-2d-mars/src/controller.test.ts
@@ -7,5 +7,29 @@ describe("controller", () => {
       const result = parseFirstLine(input);
       expect(result).toEqual({ maxX: 5, maxY: 3 });
     });
+
+    it("should reject negative number", () => {
+      const input = "-5 3";
+      const result = parseFirstLine(input);
+      expect(result).toEqual(undefined);
+    });
+
+    it("should reject empty string", () => {
+      const input = "";
+      const result = parseFirstLine(input);
+      expect(result).toEqual(undefined);
+    });
+
+    it("should reject decimal point number", () => {
+      const input = "5.5 3";
+      const result = parseFirstLine(input);
+      expect(result).toEqual(undefined);
+    });
+
+    it("should reject lines with multiple numbers", () => {
+      const input = "5 3 3";
+      const result = parseFirstLine(input);
+      expect(result).toEqual(undefined);
+    });
   });
 });
